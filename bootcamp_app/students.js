5192b70e0b2db9de61a122a9b4060612ca8f258b
Lubi Islam <lubaba.islam91@gmail.com>
2024-04-17 22:39:19 -0400

    Insert const values to replace process.argv

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,6 +7,10 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
 pool
   .query(
     `
@@ -15,7 +19,7 @@ FROM students
 JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name LIKE $1
 LIMIT $2;
-`
+`, values
   )
   .then((res) => {
     res.rows.forEach((user) => {
