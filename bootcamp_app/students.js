0f401bb2fdef29ed2e1af0c7bde7dcd13fc4676c
Lubi Islam <lubaba.islam91@gmail.com>
2024-04-17 22:04:29 -0400

    Add parameterized queries to pool.query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -13,8 +13,8 @@ pool
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
+WHERE cohorts.name LIKE $1
+LIMIT $2;
 `
   )
   .then((res) => {
