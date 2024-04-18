a34a2b912cacf2aa244e1c421b29ae8326448140
Lubi Islam <lubaba.islam91@gmail.com>
2024-04-17 22:04:37 -0400

    Add parameterized queries to pool.query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -13,7 +13,7 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1 || 'JUL02'}'
 ORDER BY teacher;
 `)
 .then(res => {
